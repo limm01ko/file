1356e33b7c1596accc3520ba95623c72d477d232
limm01ko <limm01ko@gmail.com>
2025-09-01 18:14:35 -0300

    Primeiro commit

---
+++ file.js
@@ -0,0 +1,42 @@
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { dirname, basename, extname } from 'path';
+// to use xml
+// Dependencies: "fast-xml-parser", "xmlbuilder2"
+// Install:
+//    npm install fast-xml-parser
+//    npm install xmlbuilder2
+// import { XMLParser } from "fast-xml-parser";
+// import { create } from "xmlbuilder2"; 
+export default class File {
+    constructor({ filePath }) {
+        this.file = filePath;
+        this.propretery = {
+            path: existsSync(dirname(this.file)) ? dirname(this.file) : false,
+            format: extname(this.file).toLocaleLowerCase(),
+        };
+    }
+    write(data, { overwrite= false}= {}) {
+        try { // if(typeof data !== 'object') throw new Error('[ ERRO]: the data must be a valid object`);
+            if(this.propretery.path) { if(overwrite || !existsSync(this.file)) { let output;
+                    switch(this.propretery.format) {// Formats [ ! ]
+                        case '.json': output= JSON.stringify(data, null, 2); break;
+                        // case '.xml': output= create(data).end({ prettyPrint: true }); break;
+                        case '.txt': output= data; break;
+                        default: output= null; };
+                    if(!output) { throw new Error(`[ ERRO]: Format ${this.propretery.format} is undefined`); }else
+                        { writeFileSync(this.file, output, 'utf-8'); return 'File saved successfully'; };
+                }else { throw new Error(`[ ERRO]: File ${basename(this.file)} already exists`) };
+            }else { throw new Error(`[ ERRO ]: Path ${this.propretery.path} does not exist`); };
+        } catch(err) { throw new Error(`[ Error writing the file ]: ${err.message}`); };
+    }
+    read() {
+        if(!existsSync(this.file)) { throw new Error(`[ ERRO]: File ${basename(this.file)} does not exist`); };
+        const content = readFileSync(this.file, 'utf-8'); let data;
+        switch (this.propretery.format) {// Formats [ ! ]
+            case '.json': data= JSON.parse(content); break;
+            // case '.xml': data= new XMLParser().parse(content); break;
+            case '.txt': data= content; break;
+            default: throw new Error(`[ ERRO]: Format ${this.propretery.format} not supported for reading`); };
+        return data;
+    }
+}
